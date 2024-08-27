de02275ae12c5e4c5a8044070f64dd11af648ec6
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-08-27 17:03:46 -0300

    Update testechunck.py

--- frontend/streamlit/testechunck.py
+++ frontend/streamlit/testechunck.py
@@ -154,8 +154,9 @@ for uploaded_file in uploaded_files:
     st.write("Tipo de conteúdo:", uploaded_file.type)
     st.write("Tamanho do arquivo:", uploaded_file.size, "bytes")
     document = trata_arquivo(uploaded_file)
-    st.write(document[:50])
-   
+    st.write(documen.type)
+             
+                
     # Chunking
     text_splitter = TokenTextSplitter(chunk_size=1024, chunk_overlap=102)
     texts = text_splitter.split_text(document)
