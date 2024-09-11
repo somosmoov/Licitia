3210d6addbc8017f5210d67de9c39ca1e4cc8456
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-09-11 16:00:53 -0300

    Update summary.py

--- frontend/streamlit/tools/summary.py
+++ frontend/streamlit/tools/summary.py
@@ -7,7 +7,8 @@ from azure.keyvault.secrets import SecretClient
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 #openai_api_key = st.text_input("OpenAI API Key", type="password")
-openai_api_key = st.secrets["OPENAI_KEY"]
+def setup_openai_api():
+    openai.api_key = st.secrets["OPENAI_KEY"]
 
 # Defina a URL do seu Key Vault
 #key_vault_url = KEY_VAULT_URL
@@ -24,7 +25,9 @@ openai_api_key = st.secrets["OPENAI_KEY"]
 
 # Create an OpenAI client.
 #client = OpenAI(api_key=retrieved_secret.value)
-client = OpenAI(api_key=openai_api_key)
+# Chamada da função de configuração
+setup_openai_api()
+#client = OpenAI(api_key=openai_api_key)
 
 # Ask the user for a question via `st.text_area`.
 #question = st.text_input(
@@ -33,7 +36,8 @@ client = OpenAI(api_key=openai_api_key)
 #    disabled=not uploaded_file,
 #)
 question = " faça um sumario do edital indicando apenas o contratante e o objeto do mesmo"
-if st.session_state.uploaded_file:
+#if st.session_state.uploaded_file:
+if 'uploaded_file' in st.session_state and st.session_state.uploaded_file:
     #st.write("uploaded_file", st.session_state.uploaded_file)
     #st.write("file_summary", st.session_state.file_summary)
     if "file_summary" not in st.session_state:
@@ -51,27 +55,26 @@ if st.session_state.uploaded_file:
         # Configuração para a streaming
         stream = client.chat.completions.create(
             #model="gpt-3.5-turbo",
-            model="gpt-4o-mini",
+            model="gpt-4-0613",
             messages=messages,
             stream=True,
         )
         # Inicializando o resumo na sessão
-        if 'summary' not in st.session_state:
-            st.session_state.summary = ""
+         # Inicializando o resumo na sessão
+        st.session_state.summary = ""
+
         # Iterando sobre a stream e acumulando as mensagens
-        for message in stream:
-            st.session_state.summary += message['choices'][0]['delta']
-            st.write(st.session_state.summary)
+        for chunk in stream:
+            if 'choices' in chunk and len(chunk['choices']) > 0:
+                delta = chunk['choices'][0]['delta'].get('content', '')
+                st.session_state.summary += delta
+                st.write(st.session_state.summary)
         st.session_state.file_summary = st.session_state.file_name
-        # Stream the response to the app using `st.write_stream`.
-        #st.session_state.summary = stream
-        #st.write_stream(st.session_state.summary)
-        #st.session_state.file_summary = st.session_state.file_name
         
     elif st.session_state.file_summary == st.session_state.file_name:
         #st.write("uploaded_file", st.session_state.uploaded_file)
         st.write("Arquivo sumarizado",st.session_state.file_summary)
-        st.write(st.session_state.summary)
+        st.write("Sumario: ", st.session_state.summary)
     else:
         st.write(" Edital em análise: ", st.session_state.file_name)
         document = st.session_state.doc
@@ -85,20 +88,19 @@ if st.session_state.uploaded_file:
         # Generate an answer using the OpenAI API.
         stream = client.chat.completions.create(
             #model="gpt-3.5-turbo",
-            model="gpt-4o-mini",
+            model="gpt-4-0613",
             messages=messages,
             stream=True,
         )
         # Inicializando o resumo na sessão
         st.session_state.summary = ""
         # Iterando sobre a stream e acumulando as mensagens
-        for message in stream:
-            st.session_state.summary += message['choices'][0]['delta']
-            st.write(st.session_state.summary)
-        st.session_state.file_summary = st.session_state.file_name      
-        # Stream the response to the app using `st.write_stream`.
-        #st.session_state.summary = stream
-        #st.write_stream(st.session_state.summary)
-        #st.session_state.file_summary = st.session_state.file_name        
+        for chunk in stream:
+            if 'choices' in chunk and len(chunk['choices']) > 0:
+                delta = chunk['choices'][0]['delta'].get('content', '')
+                st.session_state.summary += delta
+                st.write(st.session_state.summary)
+        
+        st.session_state.file_summary = st.session_state.file_name    
 else: 
     st.write(" Carregue o Edital para análise")
