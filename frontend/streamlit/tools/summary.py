3dada9ca0d339f75d6689d452ebca21de2bb0dee
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-09-11 12:59:22 -0300

    Update summary.py

--- frontend/streamlit/tools/summary.py
+++ frontend/streamlit/tools/summary.py
@@ -57,7 +57,7 @@ if st.session_state.uploaded_file:
         st.write_stream(stream)
         st.session_state.file_summary = st.session_state.file_name
         st.session_state.summary = stream
-elif st.session_state.file_summary == st.session_state.file_name)
+elif st.session_state.file_summary == st.session_state.file_name:
      st.write_stream(stream)
 else: 
     st.write(" Carregue o Edital para análise")
