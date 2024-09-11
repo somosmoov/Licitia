98061dddb55527ca0e2b55ae1cb72e327d8c9b3a
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-09-11 10:46:21 -0300

    Update streamlit_app.py

--- frontend/streamlit/streamlit_app.py
+++ frontend/streamlit/streamlit_app.py
@@ -16,7 +16,7 @@ def logout():
         st.rerun()
 
 # Inicializa variaveis de status
- st.session_state['uploaded_file'] = ""
+ st.session_state['uploaded_file'] = " "
 
 login_page = st.Page(login, title="Log in", icon=":material/login:")
 logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
