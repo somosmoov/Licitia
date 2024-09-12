f80678924daf805552192514960e77adcf0375bd
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-09-12 16:49:07 -0300

    Update analitic.py

--- frontend/streamlit/tools/analitic.py
+++ frontend/streamlit/tools/analitic.py
@@ -38,7 +38,7 @@ instructions = (
     "Se o documento não contiver as informações necessárias para responder a esta questão, "
     "basta escrever: Informação não identificada no documento. "
     "Se for fornecida uma resposta à pergunta, ela deverá ser anotada com uma citação. "
-    "Use o seguinte formato para citar passagens relevantes: {\" \"...}"
+    "Use o seguinte formato para citar passagens relevantes: {\"Ref \":...}"
 )
 #if st.session_state.uploaded_file:
 if 'uploaded_file' in st.session_state :
