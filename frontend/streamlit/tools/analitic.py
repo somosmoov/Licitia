9f8932fbf81bb657aa214c36c92c8281e0b3aa77
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-09-12 15:40:38 -0300

    Update analitic.py

--- frontend/streamlit/tools/analitic.py
+++ frontend/streamlit/tools/analitic.py
@@ -18,9 +18,10 @@ openai_api_key = st.secrets["OPENAI_KEY"]
 # Create an OpenAI client.
 #client = OpenAI(api_key=retrieved_secret.value)
 client = OpenAI(api_key=openai_api_key)
-question = " faça um sumario do edital indicando apenas o contratante e o objeto do mesmo"
+question = " Analise o edital fornecido e responda aos seguintes questionamentos: "
+instructions = "Você receberá um documento e uma pergunta. Sua tarefa é responder à pergunta usando apenas o documento fornecido e citar a(s) passagem(s) do documento usado para responder à pergunta. Se o documento não contiver as informações necessárias para responder a esta questão, basta escrever: “Informação insuficiente”. Se for fornecida uma resposta à pergunta, ela deverá ser anotada com uma citação. Use o seguinte formato para citar passagens relevantes ({"citação":…})"
 messages = [
-            {"role": "system", "content": "Você é um especialista em editais públicos brasileiros para tecnologia da informação "},
+            {"role": "system", "content": f"Você é um especialista em editais públicos brasileiros para tecnologia da informação {instructions} "},
             {"role": "user", "content": f"Here's a document: {document} \n\n---\n\n {question}"},
             {"role": "assistant", "content": "     ."}
         ]
