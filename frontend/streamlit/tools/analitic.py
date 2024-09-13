6e3a262e1f7e91dd53848cad648034785468002b
Moov Data Science <140577378+somosmoov@users.noreply.github.com>
2024-09-13 09:55:26 -0300

    Update analitic.py

--- frontend/streamlit/tools/analitic.py
+++ frontend/streamlit/tools/analitic.py
@@ -59,7 +59,7 @@ if 'uploaded_file' in st.session_state :
         messages = [
             {"role": "system", "content": f"Você é um especialista em editais públicos brasileiros para tecnologia da informação {instructions} "},
             {"role": "user", "content": f"Here's a document: {document} \n\n---\n\n {question}"},
-            {"role": "assistant", "content": " Entenda por declaração de contratos firmados como necessidade de apresentar atestados tecnicos de fornecimentos similares}
+            {"role": "assistant", "content": " Entenda por declaração de contratos firmados como necessidade de apresentar atestados tecnicos de fornecimentos similares"}
         ]
         completion = client.chat.completions.create(
             model="gpt-4o-mini",
